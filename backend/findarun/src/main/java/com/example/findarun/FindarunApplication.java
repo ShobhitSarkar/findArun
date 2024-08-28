24ffb22c3138416ca34cfee7df40cc6e9e8ad258
Shobhit Sarkar <shobhit@Shobhits-MacBook-Air-2.local>
2024-08-27 22:52:49 -0500

    init

---
+++ backend/findarun/src/main/java/com/example/findarun/FindarunApplication.java
@@ -0,0 +1,13 @@
+package com.example.findarun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindarunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindarunApplication.class, args);
+	}
+
+}
---
+++ backend/findarun/src/test/java/com/example/findarun/FindarunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findarun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindarunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
