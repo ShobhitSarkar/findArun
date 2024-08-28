809c64b63d8196971f85d0a1ee0bbb7e66f0b1ac
shobhit <shobhitsarkar02@gmail.com>
2024-08-28 10:35:36 -0500

    setting up database stuff

--- backend/findarun/src/main/java/com/example/findarun/model/Participant.java
+++ backend/findarun/src/main/java/com/example/findarun/model/Participant.java
@@ -1,16 +1,9 @@
 package com.example.findarun.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
 public class Participant extends User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int participantId; 
 
     // TODO: figure out the relationships between run club and events
 
