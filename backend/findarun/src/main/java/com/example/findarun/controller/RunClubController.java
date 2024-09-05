2143da222b6ace7de742af0e52ad5878d757d4fe
shobhit <shobhitsarkar02@gmail.com>
2024-09-04 21:54:30 -0500

    creating runclub management

--- backend/findarun/src/main/java/com/example/findarun/controller/RunClubController.java
+++ backend/findarun/src/main/java/com/example/findarun/controller/RunClubController.java
@@ -1,20 +1,119 @@
 package com.example.findarun.controller;
 
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.findarun.repository.RunClubRepository;
+import com.example.findarun.model.Event;
+import com.example.findarun.model.RunClub;
+import com.example.findarun.model.User;
+import com.example.findarun.service.RunClubService;
 
 @RestController
 @RequestMapping("/api/runclub")
 public class RunClubController {
 
     @Autowired
-    private RunClubRepository runClubRepository; 
+    RunClubService runClubService; 
 
-    // @Autowired 
-    // private RunClubService runClubService; 
+    @PostMapping
+    public ResponseEntity<RunClub> createRunClub(@RequestBody RunClub runClubDetails){
+
+        RunClub newRunClub = runClubService.createRunClub(runClubDetails); 
+
+        return ResponseEntity.ok(newRunClub); 
+    }
+
+    @GetMapping("/{runClubId}")
+    public ResponseEntity<RunClub> getRunClub(@PathVariable Long runClubId){
+
+        RunClub runclub = runClubService.getRunClub(runClubId);
+
+        return ResponseEntity.ok(runclub);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RunClub>> getAllRunClubs(){
+
+        List<RunClub> runClubs = runClubService.getAllRunClubs(); 
+
+        return ResponseEntity.ok(runClubs);
+
+    }
+
+    @PutMapping("/{runclubId}")
+    public ResponseEntity<RunClub> updateRunClub (@RequestBody RunClub runClubDetails){
+
+        RunClub updatedRunClub = runClubService.updateRunClub(runClubDetails); 
+
+        return ResponseEntity.ok(updatedRunClub);
+    }
+
+    @DeleteMapping("/{runClubId}")
+    public ResponseEntity<Void> deleteRunClub (@PathVariable Long runClubId){
+
+        runClubService.deleteRunClub(runClubId); 
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{runClubId}/addMember/{userId}")
+    public ResponseEntity<Void> addMemberToRunClub (@PathVariable Long runClubId, @PathVariable Long userId){
+
+        runClubService.addMember(runClubId, userId); 
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{runClubId}/removeMember/{userId}")
+    public ResponseEntity<Void> removeMemberFromRunClub (@PathVariable Long runClubId, @PathVariable Long userId){
+        runClubService.removeMember(runClubId, userId); 
+
+        return ResponseEntity.noContent().build(); 
+    }
+
+    @GetMapping("/{runClubId}/members")
+    public ResponseEntity<List<User>> getAllRunClubMembers (@PathVariable Long runClubId){
+        
+        List<User> runClubMembers = runClubService.getAllRunClubMembers(runClubId); 
+
+        return ResponseEntity.ok(runClubMembers);
+    }
+
+    @GetMapping("/{runClubId}/events")
+    public ResponseEntity<List<Event>> getAllRunClubEvents (@PathVariable Long runClubId){
+
+        List<Event> runClubEvents = runClubService.getAllRunClubEvents(runClubId); 
+
+        return ResponseEntity.ok(runClubEvents);
+    }
+
+    @PostMapping("/{runClubId}/events")
+    public ResponseEntity<Event> createEvent(@PathVariable Long runClubId, @RequestBody Event event) {
+        Event newEvent = runClubService.createEventForRunClub(runClubId, event);
+        return new ResponseEntity<>(newEvent, HttpStatus.CREATED);
+    }
+
+    /**
+     * TODO : other possible methods to implement : 
+     * 
+     * 1) get runclub by location 
+     * 2) get runclub statistics 
+     */
+
+
+
+
     
 
     
--- backend/findarun/src/main/java/com/example/findarun/service/RunClubService.java
+++ backend/findarun/src/main/java/com/example/findarun/service/RunClubService.java
@@ -5,6 +5,7 @@ import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.findarun.model.Event;
 import com.example.findarun.model.RunClub;
 import com.example.findarun.model.User;
 import com.example.findarun.repository.CreatorRepository;
@@ -22,81 +23,59 @@ public class RunClubService {
     private UserRepository userRepository;
 
     @Autowired
-    private EventRepository eventRepository; 
+    private EventRepository eventRepository;
 
-    /**
-     * Method for creating a runclub 
-     * @param runclub
-     * @param creatorId
-     * @return
-     */
-    public RunClub createRunClub (RunClub runclub, Long creatorId){
-
-        User creator = userRepository.findById(creatorId).orElseThrow(() -> new Exception("Creator not found"));
-
-        runclub.setCreator(creator); 
-
-        return runClubRepository.save(runclub); 
-        
+    public RunClub createRunClub(RunClub runClubDetails) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createRunClub'");
     }
 
-    /**
-     * TODO: Other methods to implement : 
-     * add a member to a run club 
-     * remove a member 
-     * get all members of the run club 
-     * 
-     * 
-     * create event for runclub 
-     * get all the events for the run club 
-     * 
-     */
-
-    /**
-     * Method to return a run club by id 
-     * @param id id of the run club we're trying to retrieve 
-     * @return the run club 
-     * @throws Exception if the run club is not found 
-     */
-    public RunClub getRunClubById (Long id) throws Exception{
-        return (runClubRepository.findById(id)).orElseThrow(() -> new Exception("No run club with id:" + "found")); 
+    public RunClub getRunClub(Long runClubId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRunClub'");
     }
 
-    /**
-     * Method to return all the run clubs 
-     * @return
-     */
-    public List<RunClub> getAllRunClubs(){
-        return runClubRepository.findAll();
+    public List<RunClub> getAllRunClubs() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllRunClubs'");
     }
 
-    /**
-     * Method to update the details of the rub club
-     * @param id
-     * @param runClubDetails
-     * @return
-     */
-    public RunClub updateRunClub (Long id, RunClub runClubDetails){
-
-        RunClub runClub = runClubRepository.findById(id).get();
-        
-        runClub.setCapacity(runClubDetails.getCapacity());
-        runClub.setRunClubLocation(runClubDetails.getRunClubLocation());
-        runClub.setRunClubName(runClubDetails.getRunClubName());
-        runClub.setSocialMedia(runClubDetails.getSocialMedia());
-
-        return runClubRepository.save(runClub);
-
+    public RunClub updateRunClub(RunClub runClubDetails) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateRunClub'");
     }
 
-    /**
-     * Method to delete a run club 
-     * @param id id of the rubclub being deleted
-     */
-    public void deleteRunClub (Long id){
-        
-        runClubRepository.deleteById(id);
+    public void deleteRunClub(Long runClubId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteRunClub'");
     }
+
+    public void addMember(Long runClubId, Long userId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addMember'");
+    }
+
+    public void removeMember(Long runClubId, Long userId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeMember'");
+    }
+
+    public List<User> getAllRunClubMembers(Long runClubId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllRunClubMembers'");
+    }
+
+    public List<Event> getAllRunClubEvents(Long runClubId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllRunClubEvents'");
+    }
+
+    public Event createEventForRunClub(Long runClubId, Event event) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createEventForRunClub'");
+    } 
+
+    
 
 
 
