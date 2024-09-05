aadec19b9e25973a5897fae9f5db1381b6fb2085
shobhit <shobhitsarkar02@gmail.com>
2024-09-04 21:14:43 -0500

    added methods for participantController

--- backend/findarun/src/main/java/com/example/findarun/controller/ParticipantController.java
+++ backend/findarun/src/main/java/com/example/findarun/controller/ParticipantController.java
@@ -1,10 +1,22 @@
 package com.example.findarun.controller;
 
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.findarun.model.Participant;
+import com.example.findarun.model.RunClub;
 import com.example.findarun.repository.ParticipantRepository;
+import com.example.findarun.service.ParticipantService;
 
 @RestController
 @RequestMapping("/api/participants")
@@ -13,7 +25,106 @@ public class ParticipantController {
     @Autowired 
     private ParticipantRepository participantRepository; 
 
-    // @Autowired
-    // private ParticipantService participantService;
+    @Autowired
+    private ParticipantService participantService;
+
+    @PostMapping
+    public ResponseEntity<Participant> createParticipant (@RequestBody Participant newParticipant){
+        Participant participant = new Participant(newParticipant);
+
+        return ResponseEntity.ok(newParticipant);
+    }
+
+    @GetMapping("/participantId}")
+    public ResponseEntity<Participant> getParticipant (@PathVariable Long participantId){
+        Participant participant = participantService.getSingleParticipant(participantId); 
+        
+        return ResponseEntity.ok(participant); 
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Participant>> getAllParticipants(){
+        List<Participant> participants = participantService.getAllParticipants(); 
+
+        return ResponseEntity.ok(participants);
+    }
+
+    @PutMapping("/{participantId}")
+    public ResponseEntity<Participant> updateParticipant (@PathVariable Long participantId, @RequestBody Participant updatedParticipant){
+
+        Participant participant = participantService.updateParticipant(participantId, updatedParticipant); 
+
+        return ResponseEntity.ok(updatedParticipant);
+
+    }
+
+    @DeleteMapping("/{participantId}")
+    public ResponseEntity<Void> deleteParticipant (@PathVariable Long participantId){
+        
+        participantService.deleteParticipant(participantId); 
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{participantId}/join/{runClubId}")
+    public ResponseEntity<Void> joinRunClub (@PathVariable Long participantId, @PathVariable Long runClubId){
+
+        participantService.joinRunClub(participantId, runClubId); 
+
+        return ResponseEntity.noContent().build(); 
+    }
+
+    @PostMapping("/{participantId}/leave/{runClubId}")
+    public ResponseEntity<Void> leaveRunClub (@PathVariable Long participantId, @PathVariable Long runClubId){
+
+        participantService.leaveRunClub(participantId, runClubId); 
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{participantId}/runClubs")
+    public ResponseEntity<List<RunClub>> getParticipantRunClubs (@PathVariable Long participantId){
+        
+        List<RunClub> runClubs = participantService.getRunClubs(participantId); 
+
+        return ResponseEntity.ok(runClubs);
+    }
+
+    @PutMapping("/{participantId}/register/{eventId}")
+    public ResponseEntity<Void> joinEvent (@PathVariable Long participantId, @PathVariable Long eventId){
+
+        participantService.joinEvent(participantId, eventId); 
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{participantId}/unregister/{eventId}")
+    public ResponseEntity<Void> leaveEvent (@PathVariable Long pariticpantId, @PathVariable Long eventId){
+
+        participantService.leaveEvent(pariticpantId, eventId); 
+
+        return ResponseEntity.noContent().build(); 
+    }
+
+    @GetMapping("/{participantId}/events")
+    public ResponseEntity<Void> joinEvent (@PathVariable Long participantId){
+
+        participantService.getParticipantEvents(participantId); 
+
+        return ResponseEntity.noContent().build(); 
+    }
+
+    /**
+     * Additional things to add : 
+     * 1) stats for participants 
+     * 2) search participants
+     */
+
+
+
+
+
+
+    
     
 }
--- backend/findarun/src/main/java/com/example/findarun/service/ParticipantService.java
+++ backend/findarun/src/main/java/com/example/findarun/service/ParticipantService.java
@@ -1,5 +1,55 @@
 package com.example.findarun.service;
 
+import java.util.List;
+
+import com.example.findarun.model.Participant;
+import com.example.findarun.model.RunClub;
+
 public class ParticipantService {
+
+    public Participant getSingleParticipant(Long participantId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSingleParticipant'");
+    }
+
+    public List<Participant> getAllParticipants() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllParticipants'");
+    }
+
+    public Participant updateParticipant(Long participantId, Participant updatedParticipant) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateParticipant'");
+    }
+
+    public void deleteParticipant(Long participantId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteParticipant'");
+    }
+
+    public void joinRunClub(Long participantId, Long runClubId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'joinRunClub'");
+    }
+
+    public void leaveRunClub(Long participantId, Long runClubId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'leaveRunClub'");
+    }
+
+    public void joinEvent(Long participantId, Long eventId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'joinEvent'");
+    }
+
+    public List<RunClub> getRunClubs(Long participantId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRunClubs'");
+    }
+
+    public void leaveEvent(Long pariticpantId, Long eventId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'leaveEvent'");
+    }
     
 }
